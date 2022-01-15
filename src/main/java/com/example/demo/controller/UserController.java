3533e9f2911950d2bd71a099bf91c39f472a1638
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2022-01-15 17:02:14 -0300

    no message

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,23 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 //import repository.UserRepository;
 
 
-
+// todo isto eh importante, esquecer
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
-@RequestMapping()
+@RequestMapping("/api")
 public class UserController {
 
-//    @Autowired
-//    private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping()
-    public String getUsers() {
-        return "dfa";
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 }
