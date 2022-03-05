abad4d49eeaa5f666630e176b613a379c2be97f2
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2022-03-05 12:48:47 -0300

    no message

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,28 +3,47 @@ package com.example.demo.controller;
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 //import repository.UserRepository;
 
 
-// todo isto eh importante, esquecer
+// TODO isto eh importante
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("users")
+    @GetMapping()
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
-    @PostMapping("users")
+    @PostMapping()
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserId(@PathVariable Long id){
+        User user = userRepository.findById(id).orElseThrow();
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElseThrow();
+
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+
+        User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
     }
 }
