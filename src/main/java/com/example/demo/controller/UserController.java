c114f523c7d14044867aefc1f1ab7879c21da45e
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2022-03-12 18:52:28 -0300

    no message

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 //import repository.UserRepository;
 
 
@@ -45,5 +47,15 @@ public class UserController {
 
         User updatedUser = userRepository.save(user);
         return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id){
+        User user = userRepository.findById(id).orElseThrow();
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
